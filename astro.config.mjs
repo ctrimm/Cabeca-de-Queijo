381497d8fa965908d9739085d5836bfff59b96da
Cory Trimm <cory.trimm@gmail.com>
2024-08-22 09:13:10 -0300

    Add sitemap-0.xml + i18n sitemap geenration

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,15 @@ export default defineConfig({
   integrations: [
     tailwind(),
     mdx(),
-    sitemap(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+          pt: 'pt-BR',
+        },
+      },
+    }),
     astroI18next(),
   ],
 });
