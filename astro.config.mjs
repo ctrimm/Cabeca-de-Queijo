e377afcf32ed9c1df7bafd2338b6b983c6161e4c
Cory Trimm <cory.trimm@gmail.com>
2025-03-31 09:16:16 -0400

    moved to native astro translation feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://CabecadeQueijo.com",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "pt-BR"],
+    routing: {
+      prefixDefaultLocale: false,
+      redirectToDefaultLocale: true
+    }
+  },
   integrations: [
     tailwind(),
     mdx(),
@@ -18,8 +25,7 @@ export default defineConfig({
           'pt-BR': 'pt-BR',
         },
       },
-    }),
-    astroI18next(),
+    })
   ],
   trailingSlash: "always",
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,282 @@
+export const languages = {
+  en: 'English',
+  'pt-BR': 'Português',
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    // Navigation
+    'nav.home': 'Home',
+    'nav.join': 'Join',
+    'nav.chapters': 'Chapters',
+    'nav.schedule': 'Schedule',
+    'nav.posts': 'Posts',
+    'nav.getInvolved': 'Get Involved',
+
+    // Hero
+    'hero.title.part1': 'The ',
+    'hero.title.part2': '(un)',
+    'hero.title.part3': 'Official ',
+    'hero.title.part4': 'Green Bay Packers ',
+    'hero.title.part5': 'Fan Club in Brasil 🇧🇷',
+    'hero.subtitle': 'Cabeça de Queijo is a fan club for the American National Football (NFL) Team - Green Bay Packers in Brasil. We are a group of fans that love the Packers and want to share our passion with other fans in Brasil.',
+    'hero.button1.text': 'Learn More',
+    'hero.button1.link': '#selling-points',
+    'hero.button2.text': 'Join Cabeça de Queijo for Free',
+    'hero.button2.link': '/join/',
+    'hero.altTag': 'Logo for Cabeça de Queijo of a Toucan wearing a Cheesehead Hat on its head',
+
+    // Next Games
+    'nextGames.title': 'Upcoming Games & Events',
+    'nextGames.time': 'Time',
+    'nextGames.opponent': 'Opponent',
+    'nextGames.event': 'Event',
+    'nextGames.in': 'In',
+    'nextGames.day': 'Day',
+    'nextGames.today': 'TODAY 🎉',
+    'nextGames.inProgress': 'IN PROGRESS',
+    'nextGames.endedToday': 'TODAY = FINAL',
+
+    // Selling Points
+    'sellingPoints.title': 'Why Should You Root For the Green Bay Packers?',
+    'sellingPoints.subtext': 'The Green Bay Packers have a rich history, filled with numerous championships and legendary players. Rooting for the Packers means joining a global community of passionate fans. Plus, who doesn\'t love cheese?',
+    'sellingPoints.richHistory.title': 'Rich History',
+    'sellingPoints.richHistory.description': 'The Green Bay Packers were founded in 1919 and have won 13 championships, more than any other team in the NFL.',
+    'sellingPoints.globalCommunity.title': 'Global Community',
+    'sellingPoints.globalCommunity.description': 'Rooting for the Packers means joining a global community of passionate fans.',
+    'sellingPoints.cheeseheads.title': 'Cheeseheads',
+    'sellingPoints.cheeseheads.description': 'Packers fans are affectionately known as \'Cheeseheads\', a fun and unique part of NFL fan culture.',
+    'sellingPoints.lambeauField.title': 'Lambeau Field',
+    'sellingPoints.lambeauField.description': 'The Packers\' home, Lambeau Field, is one of the most iconic stadiums in all of sports.',
+    'sellingPoints.ownership.title': 'Non-Profit Ownership',
+    'sellingPoints.ownership.description': 'The Packers are the only non-profit, community-owned franchise in the NFL.',
+    'sellingPoints.competitive.title': 'Competitive Team',
+    'sellingPoints.competitive.description': 'The Packers are consistently competitive and have a strong team, making their games exciting to watch.',
+
+    // CTA
+    'cta.goPackGo': 'Go. Pack. Go.',
+    'cta.subtext': 'Join a chapter near you or start your own.',
+    'cta.button1Text': 'Join a Chapter',
+    'cta.button1Link': '/join/',
+    'cta.button2Text': 'Start a Chapter',
+    'cta.button2Link': '/chapters/new/',
+
+    // Footer
+    'footer.copyright': 'Copyright',
+    'footer.rights': 'All Rights Reserved',
+    'footer.help': 'Need Help? Have Feedback? Click Here.',
+    'footer.helpLink': '/contact/',
+    'footer.playToday.linkText': 'Green Bay Packers Game Today?',
+    'footer.playToday.linkHref': '/green-bay-packers-game-today/',
+
+    // Contact
+    'contact.title': 'Contact Us',
+    'contact.subTitle': 'Have something you want to bring to our attention?',
+    'contact.guide': 'We are here to help. Below is our contact info -',
+
+    // Join Component
+    'joinComponent.title': 'Join Us 🧀 🧑 🇧🇷',
+    'joinComponent.subtitle': 'There are a few ways to be invovled with Cabeça de Queijo. You can sign up for our email newsletter to receive updates, join a local chapter\'s WhatsApp, or start a chapter.',
+    'joinComponent.altTag': 'Photo of a Lambeau Leap with a Packers player jumping into the stands',
+    'joinComponent.viewChaptersText': 'View Current Chapters',
+    'joinComponent.viewChaptersLink': '/chapters/',
+
+    // Start a Chapter
+    'startAChapter.text': 'Are you interested in starting a chapter in your area?',
+    'startAChapter.buttonText': 'Start a Chapter',
+    'startAChapter.buttonLink': '/chapters/new/',
+
+    // Join a Chapter
+    'joinAChapter.text': 'Interested in becoming a member of ',
+    'joinAChapter.buttonText': 'Join ',
+    'joinAChapter.buttonLink': '/join/',
+
+    // Chapter Stats
+    'chapterStats.brazilianCheeseheads': 'Brazilian Cheeseheads',
+    'chapterStats.join': 'Join For Free',
+    'chapterStats.chapters': 'Chapters',
+
+    // WhatsApp
+    'whatsApp.title': 'Join the Community',
+    'whatsApp.subtext': 'Click the Icon Below to Join the WhatsApp Community',
+    'whatsApp.altTag': 'Logo for WhatsApp Community',
+
+    // Member Benefits
+    'memberBenefits.title': 'What Membership Gets You',
+    'memberBenefits.subtext': 'Embrace the excitement of being a Packers fan in Brasil. Unite with fellow fans who share your passion and excitement for the legendary Green Bay Packers. Cabeça de Queijo is here to bring together the Packers\' vibrant fan community in Brasil.',
+    'memberBenefits.community.title': 'Community and Unity',
+    'memberBenefits.community.description': 'Become a vital part of a global Packers community, connecting with fans locally and worldwide through chapters and an exclusive WhatsApp group, fostering unity and camaraderie.',
+    'memberBenefits.updates.title': 'Information and Updates',
+    'memberBenefits.updates.description': 'Stay abreast of the latest Packers news, updates, and legendary stories through regular email subscriptions.',
+    'memberBenefits.events.title': 'Events and Gatherings',
+    'memberBenefits.events.description': 'Join game day gatherings and community events, where you can unite with fellow fans to celebrate and support the team.',
+    'memberBenefits.content.title': 'Exclusive Content',
+    'memberBenefits.content.description': 'Dive into the unique Cheesehead culture with exclusive content and virtual tours of the iconic Lambeau Field.',
+    'memberBenefits.experiences.title': 'Unique Experiences',
+    'memberBenefits.experiences.description': 'Enjoy unique experiences tailored just for the members, including special events, exclusive merchandise, and more.',
+
+    // Blog
+    'blog.title': 'Posts & Stories From Fans',
+    'blog.desc': 'We love to hear from our Brasilian Cheeseheads. If you have a story to share, please',
+    'blog.contactUs': 'Contact Us',
+
+    // Chapter Slug
+    'chapterSlug.interested': 'Interested in becoming a member of ',
+    'chapterSlug.joinWhatsApp': 'Join on WhatsApp',
+    'chapterSlug.whereToCheer': 'Where to Cheer',
+    'chapterSlug.backToChapters': 'Back to Chapters',
+    'chapterSlug.backToChaptersLink': '/chapters/',
+    'chapterSlug.placesToWatch': 'Different Places to Watch',
+    'chapterSlug.members': 'Members',
+
+    // Logos
+    'logos.text': "Plus, the team's",
+    'logos.colors': 'colors',
+    'logos.are': 'are',
+    'logos.incredible': 'incredible.',
+
+    // Misc
+    '404.title': 'Page Not Found',
+    'banner.text': '🇧🇷🧀 In Brazil? Join Our WhatsApp Group to Talk With Other Packers Fans',
+    'banner.cta': 'Join Now ↗️'
+  },
+  'pt-BR': {
+    // Navigation
+    'nav.home': 'Início',
+    'nav.join': 'Participe',
+    'nav.chapters': 'Capítulos',
+    'nav.schedule': 'Partidas',
+    'nav.posts': 'Blog',
+    'nav.getInvolved': 'Participe',
+
+    // Hero
+    'hero.title.part1': 'O ',
+    'hero.title.part2': '(não)',
+    'hero.title.part3': 'oficial ',
+    'hero.title.part4': 'Green Bay Packers ',
+    'hero.title.part5': 'no Brasil 🇧🇷',
+    'hero.subtitle': 'Cabeça de Queijo é um fã-clube do time de futebol americano (NFL) Green Bay Packers no Brasil. Somos um grupo de torcedores que amam os Packers e querem compartilhar nossa paixão com outros torcedores do Brasil.',
+    'hero.button1.text': 'Saber mais',
+    'hero.button1.link': '/pt-BR#selling-points',
+    'hero.button2.text': 'Junte-se ao Cabeça de Queijo Gratuitamente',
+    'hero.button2.link': '/pt-BR/join/',
+    'hero.altTag': 'Logotipo do Cabeça de Queijo com um Tucano usando um Chapéu de Queijo na cabeça',
+
+    // Next Games
+    'nextGames.title': 'Próximos Jogos & Eventos',
+    'nextGames.time': 'Horário',
+    'nextGames.opponent': 'Adversário',
+    'nextGames.event': 'Evento',
+    'nextGames.in': 'Em',
+    'nextGames.day': 'Dia',
+    'nextGames.today': 'HOJE 🎉',
+    'nextGames.inProgress': 'EM ANDAMENTO',
+    'nextGames.endedToday': 'HOJE = FINALIZADO',
+
+    // Selling Points
+    'sellingPoints.title': 'Por que você deveria torcer pelo Green Bay Packers?',
+    'sellingPoints.subtext': 'O Green Bay Packers tem uma história rica, repleta de inúmeros campeonatos e jogadores lendários. Torcer pelos Packers significa fazer parte de uma comunidade global de torcedores apaixonados. Além disso, quem não gosta de queijo?',
+    'sellingPoints.richHistory.title': 'História Rica',
+    'sellingPoints.richHistory.description': 'O Green Bay Packers foi fundado em 1919 e conquistou 13 campeonatos, mais do que qualquer outro time da NFL.',
+    'sellingPoints.globalCommunity.title': 'Comunidade Global',
+    'sellingPoints.globalCommunity.description': 'Torcer pelos Packers significa fazer parte de uma comunidade global de torcedores apaixonados.',
+    'sellingPoints.cheeseheads.title': 'Cheeseheads',
+    'sellingPoints.cheeseheads.description': 'Os torcedores dos Packers são carinhosamente conhecidos como \'Cheeseheads\', uma parte divertida e única da cultura dos torcedores da NFL.',
+    'sellingPoints.lambeauField.title': 'Lambeau Field',
+    'sellingPoints.lambeauField.description': 'A casa dos Packers, Lambeau Field, é um dos estádios mais icônicos de todos os esportes.',
+    'sellingPoints.ownership.title': 'Propriedade Comunitária',
+    'sellingPoints.ownership.description': 'Os Packers são a única franquia sem fins lucrativos de propriedade comunitária da NFL.',
+    'sellingPoints.competitive.title': 'Time Competitivo',
+    'sellingPoints.competitive.description': 'Os Packers são consistentemente competitivos e têm um time forte, tornando seus jogos emocionantes de assistir.',
+
+    // CTA
+    'cta.goPackGo': 'Vai. Pack. Vai.',
+    'cta.subtext': 'Participe de um capítulo perto de você ou comece o seu próprio.',
+    'cta.button1Text': 'Participar de um capítulo',
+    'cta.button1Link': '/pt-BR/join/',
+    'cta.button2Text': 'Iniciar um capítulo',
+    'cta.button2Link': '/pt-BR/chapters/new/',
+
+    // Footer
+    'footer.copyright': 'Direitos Autorais',
+    'footer.rights': 'Todos os direitos reservados',
+    'footer.help': 'Precisa de ajuda? Tem feedback? Clique aqui.',
+    'footer.helpLink': '/pt-BR/contact/',
+    'footer.playToday.linkText': 'Tem Jogo do Green Bay Packers Hoje?',
+    'footer.playToday.linkHref': '/pt-BR/green-bay-packers-game-today/',
+
+    // Contact
+    'contact.title': 'Fale Conosco',
+    'contact.subTitle': 'Tem algo que você gostaria de nos dizer?',
+    'contact.guide': 'Estamos aqui para ajudar. Abaixo estão nossas informações de contato:',
+
+    // Join Component
+    'joinComponent.title': 'Junte-se a Nós 🧀 🧑 🇧🇷',
+    'joinComponent.subtitle': 'Existem várias formas de participar do Cabeça de Queijo. Você pode se inscrever em nossa newsletter por e-mail para receber atualizações, participar do WhatsApp de um capítulo local ou iniciar um capítulo.',
+    'joinComponent.altTag': 'Foto de um Lambeau Leap com um jogador dos Packers pulando na arquibancada',
+    'joinComponent.viewChaptersText': 'Ver Capítulos Atuais',
+    'joinComponent.viewChaptersLink': '/pt-BR/chapters/',
+
+    // Start a Chapter
+    'startAChapter.text': 'Você tem interesse em iniciar um capítulo na sua região?',
+    'startAChapter.buttonText': 'Iniciar um capítulo',
+    'startAChapter.buttonLink': '/pt-BR/chapters/new/',
+
+    // Join a Chapter
+    'joinAChapter.text': 'Interessado em se tornar membro deste ',
+    'joinAChapter.buttonText': 'Participe do ',
+    'joinAChapter.buttonLink': '/pt-BR/join/',
+
+    // Chapter Stats
+    'chapterStats.brazilianCheeseheads': 'Cheeseheads Brasileiros',
+    'chapterStats.join': 'Participe Gratuitamente',
+    'chapterStats.chapters': 'Capítulos',
+
+    // WhatsApp
+    'whatsApp.title': 'Junte-se à Comunidade',
+    'whatsApp.subtext': 'Clique no ícone abaixo para participar da comunidade no WhatsApp',
+    'whatsApp.altTag': 'Logo da comunidade no WhatsApp',
+
+    // Member Benefits
+    'memberBenefits.title': 'Benefícios dos Membros',
+    'memberBenefits.subtext': 'Viva a emoção de ser um torcedor dos Packers no Brasil. Una-se a outros fãs que compartilham sua paixão e entusiasmo pelo lendário Green Bay Packers. O Cabeça de Queijo está aqui para reunir a vibrante comunidade de torcedores dos Packers no Brasil.',
+    'memberBenefits.community.title': 'Comunidade e União',
+    'memberBenefits.community.description': 'Torne-se parte vital da comunidade global dos Packers, conectando-se com torcedores local e mundialmente através de capítulos e um grupo exclusivo no WhatsApp, promovendo união e camaradagem.',
+    'memberBenefits.updates.title': 'Informações e Atualizações',
+    'memberBenefits.updates.description': 'Mantenha-se atualizado com as últimas notícias, atualizações e histórias lendárias dos Packers através de nossa newsletter.',
+    'memberBenefits.events.title': 'Eventos e Encontros',
+    'memberBenefits.events.description': 'Participe de encontros em dias de jogo e eventos comunitários, onde você pode se unir a outros torcedores para celebrar e apoiar o time.',
+    'memberBenefits.content.title': 'Conteúdo Exclusivo',
+    'memberBenefits.content.description': 'Mergulhe na cultura única dos Cheeseheads com conteúdo exclusivo e tours virtuais pelo icônico Lambeau Field.',
+    'memberBenefits.experiences.title': 'Experiências Únicas',
+    'memberBenefits.experiences.description': 'Aproveite experiências exclusivas criadas especialmente para os membros, incluindo eventos especiais, produtos exclusivos e muito mais.',
+
+    // Blog
+    'blog.title': 'Posts e Histórias dos Torcedores',
+    'blog.desc': 'Adoramos ouvir nossos Cheeseheads Brasileiros. Se você tem uma história para compartilhar,',
+    'blog.contactUs': 'Fale Conosco',
+
+    // Chapter Slug
+    'chapterSlug.interested': 'Interessado em se tornar membro do',
+    'chapterSlug.joinWhatsApp': 'Participe',
+    'chapterSlug.whereToCheer': 'Onde torcer',
+    'chapterSlug.backToChapters': 'Voltar para Capítulos',
+    'chapterSlug.backToChaptersLink': '/pt-BR/chapters/',
+    'chapterSlug.placesToWatch': 'Lugares para assistir',
+    'chapterSlug.members': 'Membros',
+
+    // Logos
+    'logos.text': 'Além disso, as',
+    'logos.colors': 'cores',
+    'logos.are': 'do time são',
+    'logos.incredible': 'incríveis.',
+
+    // Misc
+    '404.title': 'Página não encontrada',
+    'banner.text': '🇧🇷🧀 Vc esta na Brasil? Junte-se ao nosso grupo do WhatsApp para conversar com outros torcedores do Packers',
+    'banner.cta': 'Junte-se agora ↗️'
+  }
+} as const;
