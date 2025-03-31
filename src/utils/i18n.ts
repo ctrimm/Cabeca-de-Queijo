353ba48f9290e0707c35837e0ea961b0b252f025
Cory Trimm <cory.trimm@gmail.com>
2025-03-30 21:38:52 -0400

    take 101x

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,17 +1,13 @@
 import i18next from 'i18next';
-import { initI18next } from 'astro-i18next';
 
 export const initI18n = async () => {
   if (!i18next.isInitialized) {
-    await initI18next(i18next, {
+    await i18next.init({
       lng: 'en',
       fallbackLng: 'en',
       supportedLngs: ['en', 'pt-BR'],
       defaultNS: 'translation',
-      ns: ['translation'],
-      backend: {
-        loadPath: '/locales/{{lng}}/{{ns}}.json',
-      },
+      ns: ['translation']
     });
   }
   return i18next;
